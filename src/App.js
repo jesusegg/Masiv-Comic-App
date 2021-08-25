de1e9520b4e7a0891998e5ed3330e05958448949
jesusegg <jesusegg@hotmail.com>
2021-08-24 23:54:05 -0500

    fix-cors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import img from "./BEST COMIC TITLE.png";
+import { IoMdRefreshCircle } from "react-icons/io";
+import Stars from "./components/Stars";
+import { match } from "minimatch";
 
 function App() {
+  const [comic, setComic] = useState();
+  const [random, setRandom] = useState();
+
+  const page = () => setRandom(Math.round(Math.random() * 600));
+
+  const proxy = "https://cors-anywhere.herokuapp.com";
+
+  if (!random) {
+    page();
+  }
+
+  useEffect(() => {
+    random &&
+      fetch(`${proxy}/xkcd.com/${random}/info.0.json`, {
+        headers: {
+          "X-Requested-With": "wololo",
+        },
+      })
+        .then((res) => res.json())
+        .then((res) => setComic(res))
+        .catch((err) => console.log(err));
+  }, [random]);
+
+  console.log(random);
+  console.log(comic);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img className="logo" src={img} alt="best comic title" />
+      <nav className="nav">
+        <p>Vol.1</p>
+      </nav>
+      <main>
+        <div className="container_1">
+          <img
+            className="image_comic"
+            src={comic ? comic.img : ""}
+            alt={comic ? comic.alt : ""}
+          />
+          <button onClick={() => page()} className="button_refresh">
+            <IoMdRefreshCircle className="spin" />
+          </button>
+        </div>
+        <div className="container_2">
+          <p>tile</p>
+          <p>año</p>
+          <Stars />
+        </div>
+      </main>
+      <footer className="nav footer">
+        <p>Rate the comic!</p>
+      </footer>
     </div>
   );
 }
---
+++ src/components/Stars.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Rating from "@material-ui/lab/Rating";
+import Box from "@material-ui/core/Box";
+
+const labels = {
+  0.5: "Don't waste your time",
+  1: "Awful",
+  1.5: "Bad",
+  2: "Deficient",
+  2.5: "Could be better",
+  3: "Ok",
+  3.5: "Good",
+  4: "Great",
+  4.5: "Excellent",
+  5: "Excellent++",
+};
+
+const useStyles = makeStyles({
+  root: {
+    width: 300,
+    display: "grid",
+    alignItems: "center",
+  },
+});
+
+export default function HoverRating() {
+  const [value, setValue] = React.useState(0);
+  const [hover, setHover] = React.useState(-1);
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Rating
+        name="hover-feedback"
+        value={value}
+        precision={0.5}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        onChangeActive={(event, newHover) => {
+          setHover(newHover);
+        }}
+      />
+      {value !== null && (
+        <Box ml={2}>{labels[hover !== -1 ? hover : value]}</Box>
+      )}
+    </div>
+  );
+}
